baeb59038f6024910673d4445ddfbf3957cfb9fd
evanjmg <evanjmg@gmail.com>
2015-09-07 16:22:35 +0100

    tesco's server is giving me a 200 but sending me to the wrong route

--- controllers/search.js
+++ controllers/search.js
@@ -8,9 +8,12 @@ var request = require('request');
 
 var TescoProductSearch = function (query, callback) {
 
-  request('https://secure.techfortesco.com/tescolabsapi/restservice.aspx?command=LOGIN&email=&password=&developerkey='+ process.env.TESCO_API_ACCOUNT_KEY+ '&applicationkey='+ process.env.TESCO_API_APP_KEY, function (err, resp, b) {
-    request('https://secure.techfortesco.com/tescolabsapi/restservice.aspx?command=PRODUCTSEARCH&searchtext='+ query +'&page=1&sessionkey='+JSON.parse(b).SessionKey, function (error, response, body) {
+  request('https://secure.techfortesco.com/tescolabsapi/restservice.aspx?command=LOGIN&email=&developerkey='+ process.env.TESCO_API_ACCOUNT_KEY+ '&applicationkey='+ process.env.TESCO_API_APP_KEY, function (err, resp, b) {
+    console.log(resp)
+    request('https://secure.techfortesco.com/tescolabsapi/restservice.aspx?command=PRODUCTSEARCH&searchtext='+ query +'&page=1&sessionkey='+(JSON.parse(b)).SessionKey, function (error, response, body) {
+      if (error) console.log(error);
       if (!error && response.statusCode == 200) {
+
        callback(body); }
      });
   })
@@ -25,6 +28,7 @@ router.get('/', function(req,res) {
 // results
 router.get('/:query', function (req,res) {
   TescoProductSearch(req.params.query, function (response) {
+    console.log(response);
     var TescoResponse = JSON.parse(response);
       res.render('./search/results', { tescoproducts: TescoResponse.Products, message: res.locals.sessionFlash }) });
 });
